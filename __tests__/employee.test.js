6d45b10085bff900bdcca2515c8f1a0cf7e88999
nidhi <inidhi99@gmail.com>
2022-12-14 14:57:16 -0500

    updated employee.test

--- __tests__/employee.test.js
+++ __tests__/employee.test.js
@@ -1,32 +1,17 @@
-const Employee = require("../lib/employee");
+const Employee = require("../lib/Employee")
 
-
-    // describing test 
-describe("Initialization", () =>{
-    // functionilty test
-    it ("should make an object with name, id & email properties", () => {
-        let newObj = {
-            name:"nidhi", 
-            id: "1",
-            email: "inidhi99@gmail.com",
-        }
-
-    //Arrange 
-    let empObj = new Employee(newObj.name, newObj.id, newObj.email);
-    let empObj2 = new Employee ("sachin", "2", "sachin@gmail.com" )
-console.log(empObj);
-
-
-    //Act 
-
-    //Assert 
-    // expect will be test result 
-    expect(empObj.name).toBe("nidhi");
-    expect(empObj.id).toBe("1")
-    expect(empObj.getName()).toBe("sachin");
-    expect(empObj.getRole()).toBe("Employee");
+describe("Employee Class", () => {
+    describe("Initialization", () => {
+        it("should make an object with name, id & email properties", () => {
+            const employee = new Employee('Nidhi', 50, 'username@gmail.com');
+            expect(employee.name).toEqual(expect.any(String));
+            expect(employee.id).toEqual(expect.any(Number));
+            expect(employee.email).toEqual(expect.any(String));
+        })
+        
+        it("should retrive the employee's role", () => {
+            const employee = new Employee('Nidhi1', 45, 'username1@gmail.com');
+            expect(employee.getRole()).toEqual("Employee");
+        }); 
     })
 })
-
-
-
